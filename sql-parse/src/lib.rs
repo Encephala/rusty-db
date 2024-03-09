cc732c903d8d30606a9067f9ad4afbef1f2fc33b
Encephala <jonathan_rietveld@hotmail.com>
2024-03-09 23:06:04 +0100

    Expand test to build intuition

--- sql-parse/src/lib.rs
+++ sql-parse/src/lib.rs
@@ -16,15 +16,17 @@ mod tests {
     fn parse_basic_select_statement() {
         let parser = Keyword { literal: "SELECT".into() }
             .then(Whitespace.all())
-            .then(Letter.all().or(Literal { literal: '*' }))
+            .then(Letter.all().or(Literal { literal: '*' })
+                .then(Literal { literal: ',' }.then(Letter.all()).any())
+            )
             .then(Whitespace.all())
             .then(Keyword { literal: "FROM".into() })
             .then(Whitespace.all())
             .then(Letter.all())
-            .then(Literal { literal: ';' });
+            .then(Literal { literal: ';' }.any());
 
         assert_eq!(parser.parse("SELECT * FROM blabla;".into()), Some(("SELECT * FROM blabla;".into(), "".into())));
-        assert_eq!(parser.parse("SELECT asdf FROM other".into()), None);
+        assert_eq!(parser.parse("SELECT asdf,fdsa FROM other".into()), Some(("SELECT asdf,fdsa FROM other".into(), "".into())));
         assert_eq!(parser.parse("SELECT *asdf FROM blabla;".into()), None);
     }
 }
