7dda6a74a031a36ee78cbe3393d23bbb14464e26
Encephala <jonathan_rietveld@hotmail.com>
2024-06-03 13:44:45 +0200

    Basic public interface for parsing

--- sql-parse/src/lib.rs
+++ sql-parse/src/lib.rs
@@ -3,11 +3,40 @@
 mod lexer;
 pub mod parser;
 
-pub fn parse(input: &str) {
-    let _tokens = lexer::Lexer::lex(input);
+use lexer::{Token, Lexer};
+use parser::{Statement, StatementParser, Create, Insert, Select};
 
-    // TODO: Parsing
+pub fn parse_statement(input: &str) -> Option<Statement> {
+    let tokens = &mut &Lexer::lex(input);
+
+    return match tokens.get(0)? {
+        Token::Create => Create.parse(tokens),
+        Token::Insert => Insert.parse(tokens),
+        Token::Select => Select.parse(tokens),
+        _ => None,
+    };
 }
 
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_statements_basic() {
+        let inputs = [
+            ("SELECT * FROM blabla;"),
+            ("SELECT * FROM blabla WHERE x = 5;"),
+            ("CREATE TABLE blabla;"),
+            ("CREATE DATABASE xd;"),
+            ("INSERT INTO blabla VALUES ('a', 'b', 'c');"),
+        ];
+
+        inputs.iter().for_each(|test_case| {
+            let result = parse_statement(test_case);
+
+            dbg!(*test_case);
+            assert!(result.is_some());
+        })
+    }
+}
--- sql-parse/src/parser/mod.rs
+++ sql-parse/src/parser/mod.rs
@@ -2,3 +2,6 @@ mod combinators;
 mod expressions;
 mod statements;
 mod utils;
+
+pub use combinators::Chain;
+pub use statements::{StatementParser, Statement, Create, Insert, Select};
