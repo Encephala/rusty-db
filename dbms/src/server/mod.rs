e48ab44a9f8af3796f2bd073b6e190775ed6cc91
Encephala <jonathan_rietveld@hotmail.com>
2024-06-11 01:00:11 +0200

    More progress at understanding `TcpStream`s etc

--- dbms/src/server/mod.rs
+++ dbms/src/server/mod.rs
@@ -1,22 +1,49 @@
-use std::{io::{Read, Write}, net::TcpStream};
+use async_std::{
+    io::{
+        prelude::BufReadExt,
+        BufReader,
+        WriteExt
+    },
+    net::TcpStream
+};
+
+use tokio::sync::broadcast::Receiver;
 
 use crate::{Result, SqlError};
 // use sql_parse::parse_statement;
 
-pub async fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    write_welcome(&mut stream)?;
+pub async fn handle_connection(mut stream: TcpStream, mut shutdown_signal: Receiver<()>) -> Result<()> {
+    write_welcome(&mut stream).await?;
+
+    let mut reader = BufReader::new(&stream);
 
     let buf = &mut vec![];
-    stream.read_to_end(buf)
-        .map_err(SqlError::CouldNotReadFromConnection)?;
 
-    // Handle message
-    println!("Got message {}", std::str::from_utf8(buf).unwrap());
+    loop {
+        tokio::select! {
+            read_result = reader.read_until(b'\n', buf) => {
+                // Stream closed
+                if let Ok(0) = read_result {
+                    break;
+                }
+
+                // Handle read error
+                read_result.map_err(SqlError::CouldNotReadFromConnection)?;
+
+                // Handle message
+                // Dummy print for now
+                println!("Got message {}", std::str::from_utf8(buf).unwrap());
+            },
+            _ = shutdown_signal.recv() => {
+                break;
+            }
+        }
+    }
 
     return Ok(());
 }
 
-fn write_welcome(stream: &mut TcpStream) -> Result<()> {
-    return stream.write_all(&[0x48, 0x45, 0x4C, 0x4C, 0x4F])
+async fn write_welcome(stream: &mut TcpStream) -> Result<()> {
+    return stream.write_all(b"HELLO\n").await
         .map_err(SqlError::CouldNotWriteToConnection);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,100 @@
-use std::net::TcpListener;
+#![allow(clippy::needless_return)]
+use std::net::SocketAddr;
 
-use tokio::{spawn, task::JoinHandle};
-use futures::future::join_all;
+use async_std::net::{
+    TcpListener,
+    TcpStream,
+};
+use tokio::{
+    signal::ctrl_c,
+    spawn,
+    sync::broadcast::{
+        channel,
+        Receiver
+    },
+    task::JoinHandle,
+};
+use futures::future::{select_all, join_all, OptionFuture};
 
 use dbms::handle_connection;
 
+fn spawn_new_handler(
+    listen_result: Result<(TcpStream, SocketAddr), std::io::Error>,
+    shutdown_receiver: Receiver<()>,
+) -> JoinHandle<Result<(), dbms::SqlError>> {
+    match listen_result {
+        Ok((stream, address)) => {
+            println!("New connection established from {address}");
+
+            return spawn(async move {
+                handle_connection(stream, shutdown_receiver).await
+            });
+        },
+        Err(error) => panic!("{error:?}"),
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("localhost:42069").unwrap();
+    let listener = TcpListener::bind("localhost:42069").await.unwrap();
     println!("Listening on localhost:42069 (of course)");
 
     let mut join_handles = vec![];
 
-    for stream in listener.incoming() {
-        join_handles.retain(|handle: &JoinHandle<_>| {
-            !handle.is_finished()
-        });
+    let (shutdown_sender, mut shutdown_receiver_main) = channel::<()>(1);
 
-        match stream {
-            Ok(stream) => {
-                println!("New connection established from {}", stream.peer_addr().unwrap());
-                println!("Now have {} connections", join_handles.len() + 1);
+    let shutdown_sender_main = shutdown_sender.clone();
 
-                join_handles.push(spawn(async move {
-                    handle_connection(stream).await
-                }));
+    // Catch ctrl+c signal
+    spawn(async move {
+        let result = ctrl_c().await;
+
+        if let Err(error) = result {
+            eprintln!("Error while catching SIGINT: {error}");
+            eprintln!("Sending shutdown signal to all threads anyway");
+        } else {
+            println!("Sending shutdown signal to all threads");
+        }
+
+        let transmit_result = shutdown_sender_main.send(());
+
+        if transmit_result.is_err() {
+            eprintln!("Failed to send shutdown signal")
+        }
+    });
+
+    loop {
+        let join_all_future = OptionFuture::from(
+            match join_handles.len() {
+                0 => None,
+                _ => Some(select_all(&mut join_handles)),
+            }
+        );
+
+        tokio::select! {
+            _ = shutdown_receiver_main.recv() => {
+                println!("Main thread received exit signal");
+                break;
             },
-            Err(error) => panic!("{error}"),
+
+            result = listener.accept() => {
+                join_handles.push(spawn_new_handler(result, shutdown_sender.subscribe()));
+            },
+
+            Some((result, resolved_index, _)) = join_all_future => {
+                println!("Thread number {resolved_index} stopped: {result:?}");
+
+                join_handles.remove(resolved_index);
+            }
         }
     }
 
-    join_all(join_handles).await
+    println!("Waiting for all worker threads to exit");
+
+    // TODO: unwrap is not way to go here
+    println!("results: {:?}", join_all(join_handles).await
         .into_iter()
-        .collect::<Result<Result<Vec<_>, _>, _>>().unwrap().unwrap();
+        .collect::<Result<Result<Vec<_>, _>, _>>().unwrap().unwrap());
 
     println!("Main thread exiting");
 }
