c28bbd76f19450b8e6fbeb6e73ebff0ae04e323a
Encephala <jonathan_rietveld@hotmail.com>
2024-06-18 22:48:37 +0200

    Readd test for stream read/writing

--- dbms/src/server/connection/tests.rs
+++ dbms/src/server/connection/tests.rs
@@ -6,26 +6,35 @@ use tokio::{
     },
 };
 
-use super::Message;
+use crate::serialisation::{SerialisationManager, Serialiser};
+
+use super::{Message, MessageBody};
 
 // Only have one test actually open a listener,
 // otherwise we'd have conflicts and stuff
 // #[tokio::test]
-// async fn message_read_write() {
-//     let test_message: Message = Message::from(b"deez nuts".as_slice());
+async fn message_read_write() {
+    let test_message: Message = Message::from_message_body(MessageBody::Str("deez nuts".into()));
 
-//     let listener = TcpListener::bind("localhost:12345").await.unwrap();
+    let listener = TcpListener::bind("localhost:12345").await.unwrap();
 
-//     let mut stream = TcpStream::connect("localhost:12345").await.unwrap();
+    let mut stream = TcpStream::connect("localhost:12345").await.unwrap();
 
-//     let (response_stream, _) = listener.accept().await.unwrap();
+    let (response_stream, _) = listener.accept().await.unwrap();
 
-//     test_message.write(&mut stream).await.unwrap();
+    test_message.write(&mut stream, SerialisationManager(Serialiser::V2)).await.unwrap();
 
-//     let read_message = Message::read(&mut BufReader::new(response_stream)).await.unwrap();
+    let read_message = Message::read(
+        &mut BufReader::new(response_stream),
+        Serialiser::V2
+    ).await.unwrap();
 
-//     assert_eq!(
-//         read_message.0,
-//         b"deez nuts"
-//     );
-// }
+    if let MessageBody::Str(value) = read_message.body {
+        assert_eq!(
+            value,
+            "deez nuts"
+        );
+    } else {
+        panic!("Body wrong type");
+    }
+}
